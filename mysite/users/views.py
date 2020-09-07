78d8cb0aa76bd542ff99c20d35b5fd007f672906
DennisHC <denniscthdda@gmail.com>
2020-09-06 17:28:41 -0700

    Successfully connected Registration to Backend & Added fixed-top Bootstrap for Top Navbar

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.views import View
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 
 def create_account(request):
     if request.method == 'POST':
@@ -16,10 +17,12 @@ def create_account(request):
             print("Account successfully created!")
             form_username = form.cleaned_data.get('username')
             form_password = form.cleaned_data.get('password1')
-            new_user = User(username = form_username, password = form_password)
-            new_user.save()
+            new_user_obj = User()
+            new_user_obj.username = form_username
+            new_user_obj.password = form_password
+            new_user_obj.save()
             messages.success(request, f'Account created for {form_username}!')
-        return redirect('homepage')
+            return redirect('homepage')
     else:
         form = UserCreationForm()
     return render(request, 'users/create_account.html', {'form': form})
@@ -33,22 +36,24 @@ def login(request):
         # No backend authenticated the credentials
     # is_private = request.POST.get('is_private', False);
     if request.method == 'POST':
-        # form = forms.UserRegistrationForm(request.POST)
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
+        # for key, value in request.POST.items():
+        #     print("Key: %s" % (key) )
+        #     print("Value %s" % (value) )
+        username = request.POST.get('username')
+        username = username.cleaned_data
+        password = request.POST.get('password')
+        password = password.cleaned_data
+        user = authenticate(request, username = username, password = password)
+        if user is not None:
             print("You did it!")
+            login(request, user)
+            return redirect('/success')
         else:
-            print("it didn't work")
-        #if request.POST['username'] == 'dennis':
-        #    print("YOU DID IT!!")
-        for key, value in request.POST.items():
-            print("Key: %s" % (key) )
-            print("Value %s" % (value) )
-        return HttpResponseRedirect('/success')
-
-
-    dictionary_object = {'Person1': 'Dennis', 'Person2': 'Ian'}
-    return render(request, 'users/login.html', dictionary_object)
+            print("It didn't work")
+            return redirect('homepage')
+    else:
+        dictionary_object = {'Person1': 'Dennis', 'Person2': 'Ian'}
+        return render(request, 'users/login.html', dictionary_object)
 
 
 def logout(request):
