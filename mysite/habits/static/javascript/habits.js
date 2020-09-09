12ef4c733a146ede15e5c2f37e32a32450e6a7d2
Dennis Hernandez <dennisfh@uci.edu>
2020-09-08 17:30:04 -0700

    deleting entries javascript

--- mysite/habits/static/javascript/habits.js
+++ mysite/habits/static/javascript/habits.js
@@ -19,30 +19,68 @@ let habitListDisplay = document.querySelector('#habitlist');
 
 tasklist.forEach(taskText => {
     let taskHTML = document.createElement('div');
+    let colDiv = document.createElement('div');
     let checkmark = document.createElement('input');
+    let trashBtn = document.createElement('button');
+    let img = document.createElement('img');
     let task = document.createTextNode(taskText);
 
-    checkmark.setAttribute('type', 'checkbox');
-    checkmark.className = 'mx-2';
+    checkmark.type = 'checkbox';
+    checkmark.className = 'mr-2';
+
+    colDiv.className = 'col';
+
+    img.src = '/static/images/trash.svg';
+    img.alt = 'image loading failed';
+
+    trashBtn.className = 'btn btn-secondary btn-sm mt-2 float-right';
+    trashBtn.id = 'trashBtn';
+    trashBtn.appendChild(img);
+    trashBtn.addEventListener('click', (e) =>{
+        e.preventDefault();
+        taskHTML.remove();
+    })
 
     taskHTML.className = 'input-group-text text-wrap text-left my-3 mx-1';
-    taskHTML.appendChild(checkmark);
-    taskHTML.appendChild(task);
+    taskHTML.appendChild(colDiv);
+    
+    colDiv.appendChild(checkmark);
+    colDiv.appendChild(task);
+    colDiv.appendChild(trashBtn);
 
     tasksListDisplay.appendChild(taskHTML);
 })
 
 habitlist.forEach(habitText => {
     let habitHTML = document.createElement('div');
+    let colDiv = document.createElement('div');
     let checkmark = document.createElement('input');
+    let trashBtn = document.createElement('button');
+    let img = document.createElement('img');
     let habit = document.createTextNode(habitText);
 
-    checkmark.setAttribute('type', 'checkbox');
-    checkmark.className = 'mx-2';
+    checkmark.type = 'checkbox';
+    checkmark.className = 'mr-2';
+
+    colDiv.className = 'col';
+
+    img.src = '/static/images/trash.svg';
+    img.alt = 'failed';
+
+    trashBtn.className = 'btn btn-secondary btn-sm mt-2 float-right';
+    trashBtn.id = 'trashBtn';
+    trashBtn.appendChild(img);
+    trashBtn.addEventListener('click', (e) =>{
+        e.preventDefault();
+        habitHTML.remove();
+    })
 
     habitHTML.className = 'input-group-text text-wrap text-left my-3 mx-1';
-    habitHTML.appendChild(checkmark);
-    habitHTML.appendChild(habit);
+    habitHTML.appendChild(colDiv);
+    
+    colDiv.appendChild(checkmark);
+    colDiv.appendChild(habit);
+    colDiv.appendChild(trashBtn);
 
     habitListDisplay.appendChild(habitHTML);
 })
