19fa6131e208a542139adce9aa2b4cbdb97872c1
DennisHC <denniscthdda@gmail.com>
2020-09-24 11:57:22 -0700

    Updated GET task list

--- mysite/api/views.py
+++ mysite/api/views.py
@@ -12,6 +12,7 @@ import json
 from django.core import serializers
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 # My Files
 from habits.models import TaskList, TaskItem, HabitItem, HabitList
@@ -30,7 +31,7 @@ def api_overview(request):
         "me": "Dennis"
     }
     return Response(api_urls)
-
+"""
 @api_view(['GET'])
 def task_list(request):
     # 1) Get the current user object
@@ -57,14 +58,47 @@ def task_list(request):
         'my_test': 'passed'
     }
     task_lists = TaskList.objects.all()
+    print(task_lists)
     serializer = TaskListSerializer(task_lists, many=True)
     return Response(serializer.data)
+"""
 
+# Handle no user or user_task_list exception
 @api_view(['GET'])
 def user_tasks(request, pk):
+    """
     tasks = TaskList.objects.filter(id=pk)
-    serializer = TaskListSerializer(tasks, many=True)
+    print(tasks)
+    serializer = TaskListSerializer(tasks, many=True) # Try to just get the "task_items" field
+    """ 
+
+    # 1) Get the current user object
+    try:
+        user = User.objects.get(id=pk)
+    except:
+        return Response({}, status=status.HTTP_404_NOT_FOUND)
+
+    print("The current user is: ", user)
+
+    # 2) Get the current user's TaskList through the Foreign Key (Retrieves first item in QuerySet)
+    try:
+        my_task_list_name = TaskList.objects.select_related().filter(user=user).first()
+    except:
+        return Response({}, status=status.HTTP_404_NOT_FOUND)
+
+    # 3) Get the TaskList's items
+    my_task_list = my_task_list_name.task_items.all()
+
+    serializer = TaskSerializer(my_task_list, many=True)
+
     return Response(serializer.data)
+ 
+
+
+
+
+
+
  
 @api_view(['POST'])
 def user_task_create(request):
